1e56cefd3d1c5e64ac8f4e2874539da4c604f52e
qNanna <88441660+qNanna@users.noreply.github.com>
2022-04-23 08:50:32 +0300

    Create ColorDots12.js

---
+++ ColorDots12.js
@@ -0,0 +1,22 @@
+// Selectors: ".swagger" <- by id, "#swagger" <- by class
+
+console.clear()
+console.log('Starting create particles');
+
+setTimeout(() => { 
+  const injectionTarget = document.querySelector(".info") // document.querySelector("section") || document.querySelector(".swagger-ui");
+//   const wrapperList = document.querySelectorAll('.wrapper');
+  (function(){
+ for (let i = 0; i < 20; i++) {
+    const elem = document.createElement('div');
+    elem.classList.add('particle')
+    injectionTarget.appendChild(elem);
+    // document.body.appendChild(elem);
+  }
+  })();
+}, 0);
+
+function sayText(text) {
+  console.log(text);
+}
+
